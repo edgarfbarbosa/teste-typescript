00b255e38dfcfec9ee8b8abc9d9d87ff78019bb8
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-01-12 12:14:27 -0300

    A média de idade dos alunos utilizando forEach

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
+"use strict";
 // Abaixo temos uma lista de estudantes, a faculdade precisa selecionar alguns dados dessa lista. Para cada um dos itens abaixo, crie uma função em typescript que receba a lista de alunos e que retorne o que a faculdade precisa:
 //  - Uma lista contendo a cidade e quantos alunos são dessa cidade
 //  - A média de idade dos alunos
 //  - A porcentagem de alunos acima de 21 anos
 //  - Uma lista de clubes baseados nos interesses dos alunos (ex: Leitura, Jogos, Dança) contendo todos os alunos com o interesse em comum.
-
-// Não se preocupe com apresentação do resultado das funções, elas serão avaliadas a nível de lógica, práticas de código e legibilidade.
-// Se sinta livra para usar bibliotecas úteis para iteração dos dados.
-
-type Student = {
-    id: number;
-    name: string;
-    age: number;
-    city: string;
-    hobbies: string[];
-};
-
-const students: Student[] = [
+const students = [
     {
         id: 1,
         name: 'John',
@@ -59,18 +48,11 @@ const students: Student[] = [
         hobbies: ['Reading', 'Gaming'],
     },
 ];
-
-
 //  - A média de idade dos alunos
-
-console.log('Exemplo: ' + (students[0].age + students[1].age + students[2].age) / 3)
-
-let total = 0
-
+console.log('Exemplo: ' + (students[0].age + students[1].age + students[2].age) / 3);
+let total = 0;
 students.forEach((student, i) => {
-    console.log(i, student.age)
-
-    console.log(total += student.age)
-})
-
-console.log(total / students.length)
+    console.log(i, student.age);
+    console.log(total += student.age);
+});
+console.log(total / students.length);
