5981aa0a2cd99cfa6c0710f240cf074dfa4e2333
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-01-15 00:16:19 -0300

    Array com a lista de clubes

--- src/app.js
+++ src/app.js
@@ -70,3 +70,26 @@ console.log('A média de idade dos alunos: ' + total / students.length);
 const studentsOver21 = students.filter(student => student.age > 21);
 const studentsOver21Perc = (studentsOver21.length * 100 / students.length).toFixed(2);
 console.log('A porcentagem de alunos acima de 21 anos: ' + studentsOver21Perc + '%');
+//  - Uma lista de clubes baseados nos interesses dos alunos (ex: Leitura, Jogos, Dança) contendo todos os alunos com o interesse em comum.
+const listOfClubs1 = students.map(function (student) {
+    return student.hobbies[0];
+});
+console.log(listOfClubs1);
+const listOfClubs2 = students.map(function (student) {
+    return student.hobbies[1];
+});
+console.log(listOfClubs2);
+const newListOfClubs = [];
+listOfClubs1.forEach(function (hobbies) {
+    if (!newListOfClubs.includes(hobbies)) {
+        newListOfClubs.push(hobbies);
+    }
+});
+listOfClubs2.forEach(function (hobbies) {
+    if (!newListOfClubs.includes(hobbies)) {
+        newListOfClubs.push(hobbies);
+    }
+});
+for (var i = 0; i < newListOfClubs.length; i++) {
+    console.log(newListOfClubs[i]);
+}
